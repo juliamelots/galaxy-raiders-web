a6847b34cecfdc54c201e73e9f8d1d7c9d9ba71e
Julia Melo T. dos Santos <juliamelots@usp.br>
2023-07-17 18:18:52 -0300

    Add exit placeholder
    Signed-off-by: Julia Melo T. dos Santos <juliamelots@usp.br>

---
+++ pages/exit.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="canvas">
+    <div id="deep-space" />
+  </div>
+</template>
+
+<style>
+#canvas {
+  height: calc(100vh - 4rem);
+  width: calc(100vw - 4rem);
+
+  padding: 2rem;
+
+  background-color: #36bbf5;
+  overflow: hidden;
+
+  position: relative;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+#deep-space {
+  height: calc(100% - 4rem);
+  width: calc(100% - 4rem);
+
+  background-image: url("~/assets/space.png");
+  background-origin: content-box;
+  animation: slide 3s linear infinite;
+
+  position: absolute;
+  z-index: 0;
+}
+</style>

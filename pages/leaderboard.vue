40d1b4c16d5c1e710f9a0460c91dcba08ceb5cf8
Julia Melo T. dos Santos <juliamelots@usp.br>
2023-07-17 18:08:02 -0300

    Add menu pages and leaderboard.json placeholder
    Signed-off-by: Julia Melo T. dos Santos <juliamelots@usp.br>

--- pages/leaderboard.vue
+++ pages/leaderboard.vue
@@ -1,14 +1,39 @@
 <template>
   <div id="canvas">
     <div id="deep-space" />
-    <div id="menu">
+    <div id="board">
       <h1 id="game-title">GALAXY RAIDERS</h1>
-      <button><router-link class="menu-button" to="/game">Iniciar partida</router-link></button>
-      <button><router-link class="menu-button" to="/leaderboard">Visualizar placar</router-link></button>
-      <button><router-link class="menu-button" to="/exit">Fechar jogo</router-link></button>
+      <h1 id="board-title">Leaderboard</h1>
+      <table id="board-data">
+        <thead>
+          <tr>
+            <th>Date & time</th>
+            <th>Destroyed asteroids</th>
+            <th>Score</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="data in board" :key="data.dateTime">
+            <td>{{ data.dateTime }}</td>
+            <td>{{ data.destroyedAsteroids }}</td>
+            <td>{{ data.finalScore }}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   </div>
 </template>
+
+<script>
+import JSON from '~/assets/Leaderboard.json'
+export default{
+    data(){
+        return{
+            board: JSON
+        }
+    }
+}
+</script>
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Russo+One&display=swap');
@@ -41,7 +66,7 @@
   z-index: 0;
 }
 
-#menu {
+#board {
   height: calc(100% - 4rem);
   width: calc(100% - 4rem);
   
@@ -59,13 +84,5 @@
 
 #game-title {
   color: #36bbf5;
-}
-
-.menu-button {
-  background-color: black;
-  color: orange;
-  font-family: 'Russo One', sans-serif;
-  font-size: large;
-  text-decoration: none;
 }
 </style>
